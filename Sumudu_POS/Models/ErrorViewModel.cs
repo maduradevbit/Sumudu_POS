62a0d59c6f0f30321ac8081232a2538e5950815e
pasindu dissanayake <123033890+maduradevbit@users.noreply.github.com>
2024-02-23 14:10:47 +0530

    Add project files.

---
+++ Sumudu_POS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sumudu_POS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
